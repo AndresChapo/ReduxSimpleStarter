85ab7f291581a7e0fca5412fbefa659efa50f772
Andrés Chapo <11888868+AndresChapo@users.noreply.github.com>
2023-03-01 23:08:55 -0300

    Creating search bar

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import ReactDOM from 'react-DOM';
+import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
 
-import App from './components/app';
-import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+YTSearch({key:API_KEY, term: 'surf'}, function(data){
+    console.log(data);
+});
+
+// Create component. that produce HTML
+const App = () => {
+    return (<div>
+        <SearchBar />
+    </div>
+)}
+
+// Take the component and put into the page
+ReactDOM.render(<App />, document.querySelector('.container'));
