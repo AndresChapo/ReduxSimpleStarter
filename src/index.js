6287bc88dcf40e57074183d5b4d29809f5d42cae
Andrés Chapo <11888868+AndresChapo@users.noreply.github.com>
2023-03-03 13:47:31 -0300

    Video list

--- src/index.js
+++ src/index.js
@@ -1,20 +1,29 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-DOM';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 import YTSearch from 'youtube-api-search';
 
 
-
-YTSearch({key:API_KEY, term: 'surf'}, function(data){
-    console.log(data);
-});
-
 // Create component. that produce HTML
-const App = () => {
-    return (<div>
-        <SearchBar />
-    </div>
-)}
+class App extends Component {
+    constructor(props){
+        super(props);
+        this.state = {videos: []};
+        YTSearch({key:API_KEY, term: 'surf'}, (videos) => {
+            this.setState({videos});
+        });
+    }
+
+    render(){
+        return (
+            <div>
+                <SearchBar />
+                <VideoList videos={this.state.videos} />
+            </div>
+        );
+    }
+}
 
 // Take the component and put into the page
 ReactDOM.render(<App />, document.querySelector('.container'));
