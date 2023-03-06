ec1296040eb82651a64cd213d67614d0026ee53b
Andrés Chapo <11888868+AndresChapo@users.noreply.github.com>
2023-03-05 23:23:58 -0300

    Search works

--- src/index.js
+++ src/index.js
@@ -2,24 +2,40 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-DOM';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 import YTSearch from 'youtube-api-search';
 
+const API_KEY = '';
 
 // Create component. that produce HTML
 class App extends Component {
     constructor(props){
         super(props);
-        this.state = {videos: []};
-        YTSearch({key:API_KEY, term: 'surf'}, (videos) => {
-            this.setState({videos});
-        });
+        this.state = {
+            videos: [],
+            selectedVideo: null
+        };
+        this.videoSearch('surf')
     }
+
+    videoSearch(term){
+        YTSearch({key:API_KEY, term: term}, (videos) => {
+            this.setState({
+                videos: videos,
+                selectedVideo: videos[0]
+            });
+        });
+    };
 
     render(){
         return (
             <div>
-                <SearchBar />
-                <VideoList videos={this.state.videos} />
+                <SearchBar onSearchTermChange={term => this.videoSearch(term)}/>
+                <VideoDetail  video={this.state.selectedVideo} />
+                <VideoList 
+                onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+                videos={this.state.videos} 
+                />
             </div>
         );
     }
